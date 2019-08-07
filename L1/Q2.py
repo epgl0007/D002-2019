1ae814a27cf90d3e8b1e8bfbc4ee9e142552d9b5
epgl0007 <53825239+epgl0007@users.noreply.github.com>
2019-08-07 12:31:39 +0800

    Add files via upload

---
+++ L1/Q2.py
@@ -0,0 +1,12 @@
+a = 8 * 3.57
+b = 5 + 30 * 20
+c = (5 + 30) * 20
+d = 1 + (2 + 20 * 3)/(4 * 2)
+e = 1 + 2 ** 10
+f = 29 / 4
+print(a, b, c, d, e, f)
+
+from math import *
+r = sqrt(100/pi)
+A = (2 * r + 6) ** 2
+print (r , A)
---
+++ L1/Q3.py
@@ -0,0 +1,4 @@
+from math import *
+r = input("What is the radius?")
+A = int(r) ** 2 * pi
+print (A , r)
