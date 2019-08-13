3e98c1fdb8e1f52055077f1de41a32d0dd89e0cd
epgl0007 <53825239+epgl0007@users.noreply.github.com>
2019-08-13 21:16:22 +0800

    Add files via upload

--- Q3.py
+++ Q3.py
@@ -1,2 +1,32 @@
-c ["A", "E", "I" ,"O", "U", "a", "e", "i", "o", "u"]
-r = str(input("Enter 10 words:\n")) x 10
+c = ["A", "E", "I" ,"O", "U", "a", "e", "i", "o", "u"]
+r = str(input("Enter 10 words:\n"))
+r2 = str(input("Enter 10 words:\n"))
+r3 = str(input("Enter 10 words:\n"))
+r4 = str(input("Enter 10 words:\n"))
+r5 = str(input("Enter 10 words:\n"))
+r6 = str(input("Enter 10 words:\n"))
+r7 = str(input("Enter 10 words:\n"))
+r8 = str(input("Enter 10 words:\n"))
+r9 = str(input("Enter 10 words:\n"))
+r0 = str(input("Enter 10 words:\n"))
+
+if r[0] == c:
+    print(r)
+if r2[0] == c:
+    print(r2)
+if r3[0] == c:
+    print(r3)
+if r4[0] == c:
+    print(r4)
+if r5[0] == c:
+    print(r5)
+if r6[0] == c:
+    print(r6)
+if r7[0] == c:
+    print(r7)
+if r8[0] == c:
+    print(r8)
+if r9[0] == c:
+    print(r9)
+if r0[0] == c:
+    print(r0)
---
+++ Q6.py
@@ -0,0 +1,14 @@
+from random import randint
+integer = randint(1, 100)
+number = input("Try to guess the number. \nType a number between 1 to 100.")
+if (number < integer):
+    input("Your guess is too low")
+    guess = guess + 1
+elif (number > integer):
+    input("Your guess is too high")
+    guess = guess + 1
+elif (number == integer):
+    input("Your gets the number! \n You've guessed %d times" % guess)
+
+if guess == 3:
+    print("Sorry, you lose.")
