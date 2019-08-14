2ba0298a51d0f601ab497ef42f5bfc2de0be5080
epgl0007 <53825239+epgl0007@users.noreply.github.com>
2019-08-14 17:03:25 +0800

    Add files via upload

--- Q5..py
+++ Q5..py
@@ -1,12 +1,12 @@
 def printer(secret, opened):
     i = 0
     while i < len(secret):
-
-
-        
+        if secret == opened[i]:
+            printer.append(_)
         i = i + 1
-        print()
+        print(i, end="")
 
 printer("apple", [1, 2])
 printer("orange", [0, 5])
 printer("cat", [])
+#secret = %d opened = [] _
